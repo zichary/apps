d31d1ba6cf0f1a795ae9ad1b751cc70b43d7bb0e
SimonYang <zichary.sc@gmail.com>
2019-03-16 21:20:21 +0800

    [U][變更方法名稱]

--- app-server/src/main/java/com/apps/service/UserService.java
+++ app-server/src/main/java/com/apps/service/UserService.java
@@ -2,6 +2,7 @@ package com.apps.service;
 
 import com.apps.entity.UserDao;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,18 +11,19 @@ import java.util.Optional;
  * 用戶操作介面
  *
  * @author SimonYang
+ * @version 1.0.0
  * @date 2019 /2/10
  */
-public interface UserService extends BaseService<UserDao> {
+public interface UserService extends BaseService<UserDao, String>, UserDetailsService {
 
     /**
      * 用姓名查詢用戶
      *
-     * @param name
+     * @param username
      *         String 姓名
      * @return Optional<UserDao> user by name
      */
-    Optional<UserDao> getUserByName(String name);
+    Optional<UserDao> findByUserName(String username);
 
     /**
      * 獲取最新的用戶
