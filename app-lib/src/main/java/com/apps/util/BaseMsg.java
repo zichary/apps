df704da749bb42961eec3a3f5925fe8f0002d492
SimonYang <zichary.sc@gmail.com>
2019-03-16 21:18:09 +0800

    [A][新增共用名詞庫]

---
+++ app-lib/src/main/java/com/apps/util/BaseMsg.java
@@ -0,0 +1,18 @@
+package com.apps.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 共用名詞
+ *
+ * @author SimonYang
+ * @version 1.0.0
+ * @date 2019-03-15
+ */
+@Getter
+@Setter
+public class BaseMsg {
+
+    public static final String DATA_NOT_FOUND = "查無紀錄";
+}
